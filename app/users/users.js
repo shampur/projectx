13e9f9f9aa194e3044ef1402c5bfd6a15fc1d900
Charan Shampur <cshampur@cisco.com>
2017-01-27 23:55:08 -0800

    adding bluebird

--- app/users/users.js
+++ app/users/users.js
@@ -6,10 +6,27 @@ var userModel = require('./usersmodel');
 var logger = require('../../config/logger');
 
 module.exports = {
-    getAllUsers: function(req,res){
+    getAllUsers: function(req, res, next){
+
+        userModel.find({}).then(function(users){
+            logger.info('Users retrieved successfully');
+            res.send(users).status(200);
+        }, function(err){
+            logger.error('Getting users failed');
+            return next(new Error("Getting users failed").status=500);
+        });
+        /*
         userModel.find({}, function(err, users){
-            res.send(users);
-        })
+            if(err){
+                logger.error('Getting users failed');
+                return next(new Error("Getting users failed").status=500);
+            }
+            else {
+                logger.info('Users retrieved successfully');
+                res.send(users).status(200);
+            }
+        });
+        */
     },
 
     createUser: function(req, res){
@@ -21,18 +38,58 @@ module.exports = {
             city: user.city
         });
 
+        newUser.save().then(function(){
+            logger.info('User successfully added', {username: user.username});
+            res.json(user).status(201);
+        }, function (err) {
+            logger.error('Error saving user', {username: user.username});
+            return next(new Error("Error saving user").status=500);
+        });
+        /*
         newUser.save(function (err){
-            if(err)
-                logger.info('Error saving user', {username: user.username});
-        })
-
+            if(err){
+                logger.error('Error saving user', {username: user.username});
+                return next(new Error("Error saving user").status=500);
+            }
+            else{
+                logger.warn('Error saving user', {username: user.username});
+                res.json(user).status(201);
+            }
+        });
+        */
     },
 
     deleteUser: function(req, res){
-        userModel.remove({username: req.param['userid']}, function(err, removed){
-            if(removed > 0)
-                logger.info('')
-        })
+        var userid = req.param['userid'];
+        userModel.remove({username: userid}).then(function(removed){
+            if(removed > 0){
+                logger.info('User Deleted', {username: req.param['userid']});
+                res.send(userid).status(200);
+            }
+            else{
+                logger.warn('Mongoose unable to delete user', {username: userid});
+                return next(new Error("Error deleting user").status=500);
+            }
+        }, function(err){
+            logger.error('Error with delete user endpoint');
+            return next(new Error("Error with delete user endpoint").status=500);
+        });
+        /*
+        userModel.remove({username: userid}, function(err, removed){
+            if(err){
+                logger.error('Error with delete user endpoint');
+                return next(new Error("Error with delete user endpoint").status=500);
+            }
+            if(removed > 0){
+                logger.info('User Deleted', {username: req.param['userid']});
+                res.send(userid).status(200);
+            }
+            else{
+                logger.warn('User Delete Failed', {username: userid});
+                return next(new Error("Error deleting user").status=500);
+            }
+        });
+        */
     },
 
     updateUser: function(req,res){
@@ -40,10 +97,26 @@ module.exports = {
     },
 
     getSingleUser: function(req,res){
-        userModel.find({username: req.param['userid']}, function(err, users){
-            if(err)
+        var userid = req.param['userid'];
 
-            res.send(users);
-        })
+        userModel.find({username: userid}).then(function(user){
+            logger.info('Single user get successfull', {username: userid});
+            res.send(user).status(200);
+        }, function(err){
+            logger.error('Error with get single user endpoint');
+            return next(new Error('Error with get single user endpoint').status = 500);
+        });
+        /*
+        userModel.find({username: userid}, function(err, user){
+            if(err){
+                logger.error('Error with get single user endpoint');
+                return next(new Error('Error with get single user endpoint').status = 500);
+            }
+            else{
+                logger.info('Single user get successfull', {username: userid});
+                res.send(user).status(200);
+            }
+        });
+        */
     }
 }
--- server.js
+++ server.js
@@ -4,17 +4,17 @@
 
 var express = require('express');
 var app = express();
-var mongose = require('mongoose');
+var mongoose = require('mongoose');
 var port = process.env.PORT || 3000;
 var database = require('./config/database');
 var bodyParser = require('body-parser');
 var morgan = require('morgan');
 var path = require('path');
 var logger = require('./config/logger');
-
+var bluebird = require('bluebird');
 // Api Endpoints
 var userRoute = require('./app/users/usersroute');
-
+mongoose.Promise = bluebird;
 mongoose.connect(database.localUrl);
 
 var db = mongoose.connection;
@@ -23,17 +23,23 @@ db.once('open', function(){
     logger.info("Mongo DB connection successfull");
 });
 
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+app.engine('html', require('ejs').renderFile);
+
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
-
 app.use('/api/users',userRoute);
-
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
-
-app.engine('html', require('ejs').renderFile);
 app.use(express.static(path.join(__dirname, 'client')));
+
+// This is an error handler for all the Internal server errors.
+app.use(function(err, req, res, next){
+    if(err.status !== '500' || err.status !== '501')
+        return next();
+
+    res.status(err.status).send(err.message || 'Internal server error');
+});
 
 app.listen(port);
 logger.info("App listening on port :"+ port);
